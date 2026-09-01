2342417ddfcab58986740fd6e164fedf201cb5f7
actools98 <actools1998@gmail.com>
2026-09-01 17:47:22 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -55,7 +55,7 @@ function isDemoExpired() {
 // ============================================================
 //  AUTENTICACIÓN
 // ============================================================
-const PASSWORD = 'BN99';
+const PASSWORD = 'MC10;
 const loginOverlay = document.getElementById('login-overlay');
 const appWrapper = document.getElementById('app-wrapper');
 const passwordInput = document.getElementById('password-input');
